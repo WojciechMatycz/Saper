d333d26bfb75197d30f36a72834fca34e456f8af
Wojtek <wojtekmatycz@gmail.com>
2018-02-23 00:50:38 +0100

    Changed main structure of project. Mistakes in FieldButton class corretcted(some methods from Component were Overwritten) All methods in Grid class added

--- src/Field.java
+++ src/Field.java
@@ -1,4 +1,65 @@
-public class Field {
-    private FiledState state;
+import javax.swing.*;
 
+public class Field {
+    private boolean isEmpty;
+    private boolean isFlagged;
+    private boolean hasBomb;
+    private int bombsNearby;
+
+    public Field()
+    {
+        isEmpty = true;
+        isFlagged = false;
+        hasBomb = false;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        isFlagged = flagged;
+    }
+
+    public boolean isBombPlanted() {
+        return hasBomb;
+    }
+
+    public int getBombsNearby() {
+        return bombsNearby;
+    }
+
+    public void setBombsNearby(int bombsNearby) {
+        isEmpty = false;
+        this.bombsNearby = bombsNearby;
+    }
+
+    public void plantBomb() {
+        this.hasBomb = true;
+        this.isEmpty = false;
+    }
+
+    public void disableBomb()
+    {
+        this.hasBomb = false;
+        this.isEmpty = true;
+    }
+
+    @Override
+    public String toString() {
+        if(hasBomb)
+            return "#";
+        if(isEmpty)
+            return " ";
+        else
+            return ""+bombsNearby;
+    }
 }
--- src/Grid.java
+++ src/Grid.java
@@ -1,19 +1,280 @@
-public class Grid {
-    private int size;
+import javax.swing.*;
+import java.util.Random;
+
+public class Grid extends JButton {
+    private int xSize;
+    private int ySize;
     private Field map[][];
 
-    public Grid(int size)
+    public Grid(int xSize, int ySize, int bombsAmount)
     {
-        this.size = size;
-        map = new Field[size][size];
+        this.xSize = xSize;
+        this.ySize = ySize;
+        map = new Field[xSize][ySize];
+        setEmpty();
+        putBombs(bombsAmount);
+        putValues();
     }
 
-    public int getSize() {
-        return size;
+    private void setEmpty() {
+        for(int i=0; i<xSize; i++)
+            for(int j=0; j<ySize; j++)
+                map[i][j] = new Field();
     }
 
-    public void setSize(int size) {
-        this.size = size;
+
+    public void putBombs(int amount)
+    {
+        Random r = new Random();
+        while(amount > 0)
+        {
+            int x = r.nextInt(xSize);
+            int y = r.nextInt(ySize);
+            if(canPlaceABombHere(x,y)) {
+                map[x][y].plantBomb();
+                amount--;
+            }
+
+        }
+    }
+
+    private boolean canPlaceABombHere(int x, int y) {
+        if(map[x][y].isBombPlanted())
+            return false;
+        if (((x==0 && y==0) || (x==0 && y==ySize-1) || (x==xSize-1 && y==ySize-1) || (x==xSize-1 && y==0)) && isCornerBlocked(x,y))
+            return false;
+        if(((y>0 && y<ySize-1) &&(x==0 || x==xSize-1)) || ((x>0 && x<xSize-1)&&(y==0 || y==ySize-1)) && isWallBlocked(x,y))
+            return false;
+        if ((x>0 && x<xSize-1 && y>0 && y<ySize-1) && isSurrounded(x,y))
+            return false;
+        else
+            return true;
+    }
+
+    private boolean isSurrounded(int x, int y) {
+        if(map[x+1][y-1].isBombPlanted() && map[x+1][y].isBombPlanted() && map[x+1][y+1].isBombPlanted() &&
+                map[x-1][y-1].isBombPlanted() && map[x-1][y].isBombPlanted() && map[x-1][y+1].isBombPlanted()
+                && map[x][y+1].isBombPlanted() && map[x][y-1].isBombPlanted())
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isWallBlocked(int x, int y) {
+        if(x==0 && map[x+1][y-1].isBombPlanted() && map[x+1][y].isBombPlanted() &&
+                map[x+1][y+1].isBombPlanted() && map[x][y-1].isBombPlanted() && map[x][y+1].isBombPlanted())
+            return true;
+
+        if(y==0 && map[x+1][y].isBombPlanted() && map[x+1][y+1].isBombPlanted() &&
+                map[x-1][y].isBombPlanted() && map[x][y+1].isBombPlanted() && map[x-1][y+1].isBombPlanted())
+            return true;
+
+        if(x==xSize-1 && map[x][y-1].isBombPlanted() && map[x][y+1].isBombPlanted() &&
+                map[x-1][y].isBombPlanted() && map[x-1][y-1].isBombPlanted() && map[x-1][y+1].isBombPlanted())
+            return true;
+
+        if(y==ySize-1 && map[x-1][y-1].isBombPlanted() && map[x][y-1].isBombPlanted() &&
+                map[x+1][y-1].isBombPlanted() && map[x-1][y].isBombPlanted() && map[x+1][y-1].isBombPlanted())
+            return true;
+
+        return false;
+    }
+
+    private boolean isCornerBlocked(int x, int y) {
+        if((x==0 && y==0) && map[x+1][y].isBombPlanted() &&
+                map[x+1][y+1].isBombPlanted() && map[x][y+1].isBombPlanted())
+            return true;
+
+        if((x==xSize-1 && y==0) && map[x][y+1].isBombPlanted() &&
+                map[x-1][y+1].isBombPlanted() && map[x-1][y].isBombPlanted())
+            return true;
+
+        if((x==0 && y==ySize-1) && map[x+1][y].isBombPlanted() &&
+                map[x+1][y-1].isBombPlanted() && map[x][y-1].isBombPlanted())
+            return true;
+
+        if((x==xSize-1 && y==ySize-1) && map[x-1][y].isBombPlanted() &&
+                map[x-1][y-1].isBombPlanted() && map[x][y-1].isBombPlanted())
+            return true;
+
+        return false;
+    }
+
+    public void putValues()
+    {
+        int bombs;
+        for(int i=0; i<xSize; i++)
+            for(int j=0; j<ySize; j++)
+                if(map[i][j].isEmpty())
+                {
+                    bombs = countBombsAround(i,j);
+                    if(bombs!=0)
+                        map[i][j].setBombsNearby(bombs);
+                }
+    }
+
+    private int countBombsAround(int x, int y) {
+        if((x==0 && y==0) || (x==0 && y==ySize-1) || (x==xSize-1 && y==ySize-1) || (x==xSize-1 && y==0))
+        {
+            return countIfCorner(x,y);
+        }
+        else if(x==0 || x==xSize-1 || y==0 || y==ySize-1)
+        {
+            return countIfWall(x,y);
+        }
+        else
+            return countIfSurrounded(x,y);
+    }
+
+    private int countIfSurrounded(int x, int y) {
+        int bombs = 0;
+        if (map[x+1][y-1].isBombPlanted())
+            bombs++;
+        if (map[x+1][y].isBombPlanted())
+            bombs++;
+        if (map[x+1][y+1].isBombPlanted())
+            bombs++;
+        if (map[x][y + 1].isBombPlanted())
+            bombs++;
+        if (map[x][y-1].isBombPlanted())
+            bombs++;
+        if (map[x-1][y-1].isBombPlanted())
+            bombs++;
+        if (map[x-1][y].isBombPlanted())
+            bombs++;
+        if (map[x-1][y+1].isBombPlanted())
+            bombs++;
+
+        return bombs;
+    }
+
+    private int countIfWall(int x, int y) {
+        int bombs = 0;
+        if(x==0)
+        {
+            if (map[x+1][y-1].isBombPlanted())
+                bombs++;
+            if (map[x+1][y].isBombPlanted())
+            bombs++;
+            if (map[x+1][y+1].isBombPlanted())
+                bombs++;
+            if (map[x][y + 1].isBombPlanted())
+                bombs++;
+            if (map[x ][y-1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+        if(x==xSize-1)
+        {
+            if (map[x-1][y-1].isBombPlanted())
+                bombs++;
+            if (map[x-1][y].isBombPlanted())
+                bombs++;
+            if (map[x-1][y+1].isBombPlanted())
+                bombs++;
+            if (map[x][y + 1].isBombPlanted())
+                bombs++;
+            if (map[x][y-1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+        if(y==0)
+        {
+            if (map[x-1][y].isBombPlanted())
+                bombs++;
+            if (map[x+1][y].isBombPlanted())
+                bombs++;
+            if (map[x-1][y+1].isBombPlanted())
+                bombs++;
+            if (map[x][y+1].isBombPlanted())
+                bombs++;
+            if (map[x+1][y+1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+        else
+        {
+            if (map[x-1][y].isBombPlanted())
+                bombs++;
+            if (map[x+1][y].isBombPlanted())
+                bombs++;
+            if (map[x-1][y-1].isBombPlanted())
+                bombs++;
+            if (map[x][y-1].isBombPlanted())
+                bombs++;
+            if (map[x+1][y-1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+    }
+
+    private int countIfCorner(int x, int y) {
+        int bombs = 0;
+        if(x==0 && y==0) {
+            if (map[x + 1][y + 1].isBombPlanted())
+                bombs++;
+            if (map[x][y + 1].isBombPlanted())
+                bombs++;
+            if (map[x + 1][y].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+        else if((x==0 && y==ySize-1))
+        {
+            if (map[x + 1][y].isBombPlanted())
+                bombs++;
+            if (map[x][y-1].isBombPlanted())
+                bombs++;
+            if (map[x][y-1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+        else if((x==xSize-1 && y==ySize-1))
+        {
+            if (map[x-1][y].isBombPlanted())
+                bombs++;
+            if (map[x][y-1].isBombPlanted())
+                bombs++;
+            if (map[x-1][y-1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+        else
+        {
+            if (map[x-1][y].isBombPlanted())
+                bombs++;
+            if (map[x-1][y+1].isBombPlanted())
+                bombs++;
+            if (map[x][y+1].isBombPlanted())
+                bombs++;
+
+            return bombs;
+        }
+
+    }
+
+
+    public int getxSize() {
+        return xSize;
+    }
+
+    public void setxSize(int xSize) {
+        this.xSize = xSize;
+    }
+
+    public int getySize() {
+        return ySize;
+    }
+
+    public void setySize(int ySize) {
+        this.ySize = ySize;
     }
 
     public Field[][] getMap() {
@@ -27,8 +288,8 @@ public class Grid {
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for (int i=0; i<size; i++) {
-            for (int j = 0; j < size; j++)
+        for (int i=0; i<xSize; i++) {
+            for (int j = 0; j < ySize; j++)
                 result.append(map[i][j]);
             result.append("\n");
         }
